1b1c768cb79ba1e48fc271b855b004fdb80b9cd0
KevL927 <leekevin@gmail.com>
2017-06-08 01:39:17 -0400

    Add listener for each click to the td and add functionality to update the game board

--- server/index.js
+++ server/index.js
@@ -9,28 +9,51 @@ app.use(express.static(__dirname + '/public'));
 
 const io = require('socket.io')(http);
 
-io.on('connection', client => {
-    addPlayerToList(client.id);
+io.on('connection', socket => {
+    addPlayerToList(socket.id);
     
-    client.on('disconnect', () => {
-    removePlayerFromList(client.id)
-})
+    socket.on('disconnect', () => {
+        removePlayerFromList(socket.id)
+    });
+
+    socket.on('click td', clickedArrayIndex => {
+        const updateGameBoardResponse = updateGameBoard(socket.id, clickedArrayIndex);
+
+        if(updateGameBoardResponse.isUpdated) {
+            socket.emit('gameboard update', updateGameBoardResponse.gameBoard);
+        }
+    });
 });
+
 
 const addPlayerToList = clientId => {
     if(db.players.length > 1) return;
 
     db.players.push(clientId);
-    console.log(db.players)
 }
 
 const removePlayerFromList = (clientId) => {
-    const idIndex = db.players.indexOf(clientId)
-console.log(idIndex)
-    if(idIndex >= 0) db.players.splice(idIndex, 1);
+    const idIndex = db.players.indexOf(clientId);
 
-    console.log(db.players)
+    if(idIndex >= 0) db.players.splice(idIndex, 1);
 }
+
+const updateGameBoard = (clientId, clickedArrayIndex) => {
+    const idIndex = db.players.indexOf(clientId);
+
+    if(idIndex === 0 && db.gameBoard[clickedArrayIndex] === null && db.isXTurn === false) {
+        db.gameBoard[clickedArrayIndex] = 'O';
+        db.isXTurn = true;
+        return { isUpdated: true, gameBoard: db.gameBoard};
+    }
+    else if(idIndex === 1 && db.gameBoard[clickedArrayIndex] === null && db.isXTurn === true) {
+        db.gameBoard[clickedArrayIndex] = 'X';
+        db.isXTurn = false;
+        return { isUpdated: true, gameBoard: db.gameBoard};
+    }
+    return { isUpdated: false };
+}
+
 
 http.listen(port, function(){
   console.log('listening on *: ' + port);
