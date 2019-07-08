b811c5cfa565dfeff30edbff028b819942e1b362
Kevin Lee <ki.lee@sba.gov>
2019-07-08 11:19:22 -0400

    console log test

--- server/index.js
+++ server/index.js
@@ -1,32 +1,34 @@
-const express = require('express');
-const app = express();
-const http = require('http').Server(app);
-const port = process.env.PORT || 3000;
+// const express = require('express');
+// const app = express();
+// const http = require('http').Server(app);
+// const port = process.env.PORT || 3000;
 
-const addPlayerToList = require('./updateDb').addPlayerToList;
-const removePlayerFromList = require('./updateDb').removePlayerFromList;
-const updateGameBoardAndCheckWinner = require('./updateDb').updateGameBoardAndCheckWinner;
+// const addPlayerToList = require('./updateDb').addPlayerToList;
+// const removePlayerFromList = require('./updateDb').removePlayerFromList;
+// const updateGameBoardAndCheckWinner = require('./updateDb').updateGameBoardAndCheckWinner;
 
-const db = require('./db');
+// const db = require('./db');
 
-app.use(express.static(__dirname + '/public'));
+// app.use(express.static(__dirname + '/public'));
 
-const io = require('socket.io')(http);
+// const io = require('socket.io')(http);
 
-io.on('connection', socket => {
-    addPlayerToList(socket.id);
-    
-    socket.on('disconnect', () => {
-        removePlayerFromList(socket.id)
-    });
+// io.on('connection', socket => {
+//     addPlayerToList(socket.id);
 
-    socket.on('click td', clickedArrayIndex => {
-        const updateGameBoardAndCheckWinnerResponse = updateGameBoardAndCheckWinner(socket.id, clickedArrayIndex);
+//     socket.on('disconnect', () => {
+//         removePlayerFromList(socket.id)
+//     });
 
-        io.emit('gameboard update', updateGameBoardAndCheckWinnerResponse);
-    });
-});
+//     socket.on('click td', clickedArrayIndex => {
+//         const updateGameBoardAndCheckWinnerResponse = updateGameBoardAndCheckWinner(socket.id, clickedArrayIndex);
 
-http.listen(port, function(){
-  console.log('listening on *: ' + port);
-});
+//         io.emit('gameboard update', updateGameBoardAndCheckWinnerResponse);
+//     });
+// });
+
+// http.listen(port, function(){
+//   console.log('listening on *: ' + port);
+// });
+
+console.log('hello world')
